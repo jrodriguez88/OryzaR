e9c14b105135a221a9a0f41c9bf948611f750b0e
Jeikosd <jeison.mesa@correounivalle.edu.co>
2016-10-18 14:15:39 -0500

    prueba generacion archivo re runs

---
+++ make_reruns.R
@@ -0,0 +1,17 @@
+FILEIT = 'VVME.exp'   # Archivo Experimental
+FILEI2 = 'VVME.sol'	  # Archivo Suelo
+FILEI1 = 'FED174.crp'			    # Archivo de Cultivo
+CNTR = 'VVME'								                # Nombre de la estacion
+ISTN = 1:99									                  # Escenario Climatico
+IYEAR =  2016								                # Año inicio Simulacion
+
+# STTIME = 275.								                # Dia de Inicio de Simulacion
+EMD =   seq(215, 300, 5)
+EMYR =  2016
+
+data_base <- expand.grid(ISTN, EMD, IYEAR)
+head(data_base)
+
+
+
+
