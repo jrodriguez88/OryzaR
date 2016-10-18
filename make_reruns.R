694cc79711dd2758f9495f34275ce3f566a20928
Jeikosd <jeison.mesa@correounivalle.edu.co>
2016-10-18 14:49:37 -0500

    add colnames

--- make_reruns.R
+++ make_reruns.R
@@ -1,26 +1,21 @@
-FILEIT = 'VVME.exp'                                         # Archivo Experimental
-FILEI2 = 'VVME.sol'	                                        # Archivo Suelo
-FILEI1 = 'FED174.crp'			                            # Archivo de Cultivo
+FILEIT = 'VVME.exp'   # Archivo Experimental
+FILEI2 = 'VVME.sol'	  # Archivo Suelo
+FILEI1 = 'FED174.crp'			    # Archivo de Cultivo
 CNTR = 'VVME'								                # Nombre de la estacion
-ISTN = 1:99									                # Escenario Climatico
+ISTN = 1:99									                  # Escenario Climatico
 IYEAR =  2016								                # Año inicio Simulacion
 
 # STTIME = 275.								                # Dia de Inicio de Simulacion
 EMD =   seq(215, 300, 5)                      # primer valor fecha de simulacion, segundo valor fin de simulacion
 EMYR =  2016
 
-
 data_base <- expand.grid(ISTN, EMD, IYEAR)
-<<<<<<< HEAD
 
 colnames(data_base) <- c('ISTN', 'EMD', 'IYEAR')
-=======
-colnames(database)
->>>>>>> 212221f5c7bee86be61bb759b60c4038571b17c5
 head(data_base)
 
 View(data_base)
 
-
+dslbasdan
 
 
